a5a07621288dab793da0854fb349982e373d0a8b
Adi1M <adikmurziev@gmail.com>
2022-12-18 01:01:34 +0600

    Today's commit and push

--- account/PasswordPage.js
+++ account/PasswordPage.js
@@ -1,18 +1,21 @@
 const email = localStorage.getItem("email");
 document.getElementById("log_email").textContent = email;
+document.getElementById('user').value = email;
 
 function login(event) {
+
     event.preventDefault();
-
     let form = event.target;
-    let formData = new FormData(form);
 
+    let formData = new FormData(form);
     fetch('http://localhost/login.php').then(result => {
         return result.text();
     }).then(text => {
         prompt(text);
     })
 }
+
+////////////////////////////////////////////////////////////////////////////////
 const passwordInput = document.querySelector('#password');
 const eye = document.querySelector('#eye');
 
@@ -22,17 +25,25 @@ eye.addEventListener('click', function () {
         'text' : 'password';
     passwordInput.setAttribute('type', type);
 });
+///////////////////////////////////////////////////////////////////////////////
 
+const formInput = document.querySelector(".form-input");
+const formButton = document.querySelector(".form-button");
 
-function changeColorOfButton() {
-    if (document.getElementById('password').value !== '') {
-        document.getElementById('nextBtn').style.backgroundColor = rgb(62, 106, 225);
+formButton.disabled = true;
+
+formInput.addEventListener("keyup", buttonState);
+function buttonState() {
+    if (document.querySelector("#password").value === "") {
+        formButton.disabled = true;
     } else {
-        document.getElementById('nextBtn').style.backgroundColor = rgb(159, 181, 240);
+        formButton.disabled = false;
+        formButton.background = rgb(62,106,225);
     }
 }
 
-const usernName = localStorage.getItem('email');
 
-document.getElementById('user').value = usernName;
+formButton.addEventListener("click", () => {
+    console.log("You entered:", document.querySelector(".form-input").value);
+});
 
